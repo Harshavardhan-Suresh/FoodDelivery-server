5fe2efc043e4ffe3f30d2b8bb3ea5df1b7e4177b
Deepak Sriram <deepak2010131@ssn.edu.in>
2022-05-16 17:14:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express =require('express')
+const app=express()
+const mysql = require('mysql')
+
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'DeepakDB1',
+    port: 3306
+})
+
+
+db.connect((err)=>{
+    if(err){
+        console.log("error occured", err)
+    }
+    else{
+        console.log("connection succesful")
+    }
+})
+
+
+app.get('/', (req, res)=>{
+    db.query('INSERT INTO TEST VALUES(100)',(err, result)=>{
+        res.send('test')
+    })
+
+})
+
+
+app.listen(3000, ()=>{
+    console.log('working')
+})
