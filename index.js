5929af148ccb6f37049e943a58566fa5549c0e7b
Deepak Sriram <deepak2010131@ssn.edu.in>
2022-05-17 19:28:47 +0530

    before adding status

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express =require('express')
 const app=express()
 const mysql = require('mysql2')
-
+const cors=require('cors')
+const bodyParser=require('body-parser')
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -10,7 +11,6 @@ const db = mysql.createConnection({
     database: 'FoodSystem',
     port: 3306
 })
-
 
 db.connect((err)=>{
     if(err){
@@ -21,21 +21,83 @@ db.connect((err)=>{
     }
 })
 
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors())
 
 app.get('/', (req, res)=>{
-    res.send('homepage');
-
+    res.send('homepage')
 })
 
-// app.get('/:num/:num2', (req, res)=>{
-//     db.query('INSERT INTO TEST VALUES(?)',[req.params.num], (err, result)=>{
-//         console.log('one insert')
-//     })
-//     db.query('INSERT INTO TEST VALUES(?)',[req.params.num2], (err, result)=>{
-//         console.log('two insert')
-//         res.send('second insert successful')
-//     })
-// })
+app.get('/restaurants', (req, res)=>{
+    db.query('SELECT * FROM RESTAURANT', (err, result)=>{
+        res.send(result)
+    })
+})
+
+app.get('/orders', (req, res)=>{
+    db.query('SELECT * FROM ORDERS', (err, result)=>{
+        res.send(result)
+    })
+})
+
+app.put('/customer/cart/:id/:food_name', (req, res)=>{
+    db.query('UPDATE CART SET QUANTITY=? WHERE CUSTOMER_ID=? AND FOOD_NAME=?', [req.body.quantity, req.params.id, req.params.food_name],(err, result)=>{
+        res.send('quantity updated')
+    })
+})
+
+app.delete('/customer/cart/:id/:food_name', (req, res)=>{
+    console.log('delete request received')
+    db.query('DELETE FROM CART WHERE CUSTOMER_ID=? AND FOOD_NAME=?', [req.params.id, req.params.food_name], (err, result)=>{
+        res.send('removed item from cart')
+    })
+})
+
+app.get('/customer/cart/:id', (req, res)=>{
+    db.query('SELECT * FROM CART WHERE CUSTOMER_ID=?', [req.params.id], (err, result)=>{
+        res.send(result);
+    })
+})
+
+app.post('/customer/cart/:id', (req, res)=>{
+    db.query('INSERT INTO CART (CUSTOMER_ID, FOOD_NAME, QUANTITY) VALUES (?,?,?)', [req.params.id, req.body.food_name, req.body.quantity],(err, result)=>{
+        res.send('added to cart')
+    })
+})
+
+app.delete('/customer/cart/:id', (req, res)=>{
+    // console.log('new delete request')
+    // res.send('deleted successfully')
+    db.query('DELETE FROM CART WHERE CUSTOMER_ID=?', [req.params.id], (err, result)=>{
+        res.send('deleted successfully')
+    })
+})
+
+app.get('/customer/orders/:id', (req, res)=>{
+    db.query('SELECT * FROM ORDERS WHERE CUSTOMER_ID=?', [req.params.id], (err, result)=>{
+        res.send(result);
+    })
+})
+
+app.post('/customer/orders/:id', (req, res)=>{
+    const {order_ID, customer_ID, employee_ID, restaurant_ID, details}=req.body
+    const st=req.body.status
+
+    db.query('INSERT INTO ORDERS (ORDER_ID, STATUS, CUSTOMER_ID, EMPLOYEE_ID, RESTAURANT_ID) values (?, ?, ?, ?, ?)', [order_ID, st, customer_ID, employee_ID, restaurant_ID], (err, result)=>{
+        details.forEach((detail)=>{
+            const {food_name, quantity}=detail
+            db.query('INSERT INTO ORDER_DETAILS (ORDER_ID, FOOD_NAME, QUANTITY) values (?,?,?)', [order_ID, food_name, quantity])
+        })
+        res.send('post succesful')
+    })
+})
+
+app.get('/employee/orders/:id', (req, res)=>{
+    db.query('SELECT * FROM ORDERS WHERE EMPLOYEE_ID=?', [req.params.id], (err, result)=>{
+        res.send(result);
+    })
+})
 
 app.get('/customer/:id', (req, res)=>{
     db.query('SELECT * FROM CUSTOMER WHERE CUSTOMER_ID=?', [req.params.id], (err, result)=>{
@@ -43,6 +105,11 @@ app.get('/customer/:id', (req, res)=>{
     })
 })
 
+app.put('/customer/:id', (req, res)=>{
+    db.query('UPDATE CUSTOMER SET RESTAURANT_ID=? WHERE CUSTOMER_ID=?', [req.body.restaurant_ID, req.params.id], (err, result)=>{
+        res.send('updated successfully')
+    })
+})
 
 app.get('/employee/:id', (req, res)=>{
     db.query('SELECT * FROM EMPLOYEE WHERE EMPLOYEE_ID=?', [req.params.id], (err, result)=>{
@@ -50,21 +117,17 @@ app.get('/employee/:id', (req, res)=>{
     })
 })
 
-app.get('/customer/order/:id', (req, res)=>{
-    db.query('SELECT * FROM ORDERS WHERE CUSTOMER_ID=?', [req.params.id], (err, result)=>{
+app.get('/foods/:id', (req, res)=>{
+    db.query('SELECT * FROM RESTAURANT_FOOD WHERE RESTAURANT_ID=?', [req.params.id], (err, result)=>{
         res.send(result);
     })
 })
 
-
-app.get('/employee/order/:id', (req, res)=>{
-    db.query('SELECT * FROM ORDERS WHERE EMPLOYEE_ID=?', [req.params.id], (err, result)=>{
-        res.send(result);
+app.put('/orders/:id', (req, res)=>{
+    db.query('UPDATE ORDERS SET STATUS=? WHERE ORDER_ID=?', [req.body.status, req.params.id], (err, result)=>{
+        res.send('Order completed')
     })
 })
-
-
-
 
 app.listen(3001, ()=>{
     console.log('working')
